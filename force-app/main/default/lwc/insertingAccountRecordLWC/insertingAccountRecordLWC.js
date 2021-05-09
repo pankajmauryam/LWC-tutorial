ccf0f6a3baea43baf8d85235cca8305614fefe85
Pankaj Maurya <pankajmauryam@gmail.com>
2021-05-09 08:39:38 +0530

    New LWC Component Created On Account Object

---
+++ force-app/main/default/lwc/insertingAccountRecordLWC/insertingAccountRecordLWC.js
@@ -0,0 +1,30 @@
+import { LightningElement,api } from 'lwc';
+import createAccountRecord from '@salesforce/apex/AccountClassController.createdName';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+export default class InsertingAccountRecordLWC extends LightningElement {
+    @api accountName;
+
+    handleChangeAccountName(event){
+        this.accountName = event.detail.value;
+    }
+    createAccount(event){
+        createAccountRecord({
+            accountNameFromText : this.accountName
+        }).then(result => {
+            const event = ShowToastEvent({
+                title : 'Account Created',
+                message : 'Account created successfully'+ this.accountName,
+                variant : success
+            })
+            this.dispatchEvent(event);
+        }).catch(error => {
+            const event= ShowToastEvent({
+                title: 'Error',
+                message : 'Please contact to admin',
+                variant : error
+            })
+            this.dispatchEvent(event);
+        })
+
+    }
+}
