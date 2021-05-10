a84a81ca18f074c5e6436d6d71201a7246c0bc0d
Pankaj Maurya <pankajmauryam@gmail.com>
2021-05-10 20:35:20 +0530

    LWC changes

--- force-app/main/default/lwc/insertingAccountRecordLWC/insertingAccountRecordLWC.js
+++ force-app/main/default/lwc/insertingAccountRecordLWC/insertingAccountRecordLWC.js
@@ -3,17 +3,22 @@ import createAccountRecord from '@salesforce/apex/AccountClassController.created
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 export default class InsertingAccountRecordLWC extends LightningElement {
     @api accountName;
+    @api mobileNumber;
 
     handleChangeAccountName(event){
         this.accountName = event.detail.value;
     }
+    handleChangeMobileNumber(event){
+        this.mobileNumber = event.detail.value;
+    }
     createAccount(event){
         createAccountRecord({
-            accountNameFromText : this.accountName
+            accountNameFromText : this.accountName,
+            accountMobileNumberFromInput : this.mobileNumber
         }).then(result => {
             const event = new ShowToastEvent({
                 title : 'Account Created',
-                message : 'Account created successfully'+ this.accountName,
+                message : 'Account created successfully'+ "Account Name"+this.accountName+"Account Mobile="+ this.mobileNumber,
                 variant : 'success'
             });
             this.dispatchEvent(event);
